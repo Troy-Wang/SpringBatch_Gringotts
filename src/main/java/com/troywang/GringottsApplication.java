4965653ab16f51abf0f2dcee10aeae0b90e1c377
Troy <wangzhijian22@gmail.com>
2018-05-14 11:42:58 +0800

    generate infrastructure with spring boot

---
+++ src/main/java/com/troywang/GringottsApplication.java
@@ -0,0 +1,12 @@
+package com.troywang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GringottsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GringottsApplication.class, args);
+	}
+}
