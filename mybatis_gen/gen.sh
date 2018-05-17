434c3519e8fe2e02763c34a527cbfbf75e951a40
Troy <wangzhijian22@gmail.com>
2018-05-17 15:15:10 +0800

    remove useless code

--- mybatis_gen/gen.sh
+++ mybatis_gen/gen.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#mkdir -p ../src/main/java/src/main/resources/META-INF/mappers/auto
-#mkdir -p ../app/dal/src/main/java
 java -cp .:mybatis-generator-core-1.3.3-SNAPSHOT-baidu.jar org.mybatis.generator.api.ShellRunner -configfile config.xml -overwrite
 echo "\033[32m Gen SUCCESSFUL!!! \033[0m"
