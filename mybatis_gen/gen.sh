28d00c89bc22c2c50d2d9e501cb6d09bb9a7329e
Troy <wangzhijian22@gmail.com>
2018-09-10 16:23:39 +0800

    modify file name

--- mybatis_gen/gen.sh
+++ mybatis_gen/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -cp .:mybatis-generator-core-1.3.3-SNAPSHOT-baidu.jar org.mybatis.generator.api.ShellRunner -configfile config.xml -overwrite
+java -cp .:mybatis-generator-core-1.3.3-SNAPSHOT.jar org.mybatis.generator.api.ShellRunner -configfile config.xml -overwrite
 echo "\033[32m Gen SUCCESSFUL!!! \033[0m"
